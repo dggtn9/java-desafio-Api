c7a684b12b2340144a6f3bb003f3738e3927fcb1
daniela <dggtn97@gmail.com>
2024-11-21 15:27:15 -0300

    modifico clase CorsConfiguration

--- demo/src/main/java/com/aluracursos/screenmatchfrases/CorsConfiguration.java
+++ demo/src/main/java/com/aluracursos/screenmatchfrases/CorsConfiguration.java
@@ -1,4 +1,4 @@
-package java.com.aluracursos.screenmatchfrases;
+package com.aluracursos.screenmatchfrases;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -9,7 +9,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5501")
+                .allowedOrigins("http://127.0.0.1:5500")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
